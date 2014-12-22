5ea4a076602a6eab08260899dc80c9c6b54c51fc
caktux <caktux@gmail.com>
2014-12-22 03:13:33 -0500

    fix versionfile_source..

--- pyepm/_version.py
+++ pyepm/_version.py
@@ -15,7 +15,7 @@ git_full = "$Format:%H$"
 # these strings are filled in when 'setup.py versioneer' creates _version.py
 tag_prefix = ""
 parentdir_prefix = "pyepm-"
-versionfile_source = "_version.py"
+versionfile_source = "pyepm/_version.py"
 
 import os, sys, re, subprocess, errno
 
