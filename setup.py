65141d1432aca94995305dcf4199a33987e179f9
caktux <caktux@gmail.com>
2014-12-24 14:43:52 -0500

    update setuptools info

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 import versioneer
 versioneer.VCS = 'git'
@@ -6,17 +8,28 @@ versioneer.versionfile_build = 'pyepm/_version.py'
 versioneer.tag_prefix = '' # tags are like 1.2.0
 versioneer.parentdir_prefix = 'pyepm-' # dirname like 'myproject-1.2.0'
 
-console_scripts = ['pyepm=pyepm.pyepm:main']
+CONSOLE_SCRIPTS = ['pyepm=pyepm.pyepm:main']
 
 setup(name="pyepm",
       packages=find_packages("."),
       description='Python Ethereum Package Manager',
+      author="caktux",
+      author_email="caktux@gmail.com",
       url='https://github.com/etherex/pyepm/',
       install_requires=[
+          'pyyaml',
           'pyethereum',
           'ethereum-serpent',
           'requests'
       ],
-      entry_points=dict(console_scripts=console_scripts),
+      entry_points=dict(console_scripts=CONSOLE_SCRIPTS),
       version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass())
+      cmdclass=versioneer.get_cmdclass(),
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Console",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python :: 2.7",
+          ])
